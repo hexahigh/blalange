e5cc5470881b9b4ea9d2fb24ed9be374ebd3d98a
Boofdev <hexahigh0@gmail.com>
2024-03-20 14:10:16 +0100

    im giving up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import svelte from '@astrojs/svelte';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), svelte(), sitemap()]
 });
